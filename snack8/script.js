f6eaf151ed9e9713acddd0950336acc17aef9ad9
emilioff8822 <emilioff@hotmail.it>
2023-03-27 14:50:15 +0200

    commit-6pt2

--- snack8/script.js
+++ snack8/script.js
@@ -4,11 +4,13 @@
 
 let numero = prompt("Inserisci un numero di 4 cifre:");
 let somma = 0;
+let messaggio = document.getElementById("risultato");
+
 
 for (var i = 0; i < numero.length; i++) {
     somma += parseInt(numero[i]);
 }
-console.log("La somma delle cifre del numero " + numero + " è: " + somma);
+messaggio.innerHTML ="La somma delle cifre del numero " + numero + " è:  " + somma;
 
 
 
